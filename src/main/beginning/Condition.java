18b3ff657192d38485f6c86a142f6e8c5dfe6fd1
Harin3Bone <harin.thananam@gmail.com>
2021-01-05 22:12:16 +0700

    Lesson5 - If-Else statement handle

--- src/main/beginning/Condition.java
+++ src/main/beginning/Condition.java
@@ -5,7 +5,9 @@ import java.util.Scanner;
 public class Condition {
     public void runCondition() {
 //        ifStatement();
-        ifElseStatement();
+//        ifElseStatement();
+//        ifMoreThanOne();
+        avoidIfMoreThanOne();
     }
 
     private void ifStatement() {
@@ -14,17 +16,17 @@ public class Condition {
         System.out.print("Input x (number) : ");
         int x = sc.nextInt();
         System.out.print("Input y (number) : ");
-        int y= sc.nextInt();
+        int y = sc.nextInt();
 
         if (x < y) {
             System.out.println("X is less than Y");
         }
     }
 
-    private void ifElseStatement(){
+    private void ifElseStatement() {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Input number : ");
+        System.out.print("Input number : ");
         int value = sc.nextInt();
 
         int init = 50;
@@ -33,6 +35,42 @@ public class Condition {
             System.out.println("Value more than Initial");
         } else {
             System.out.println("Value less than Initial");
+        }
+    }
+
+    private void ifMoreThanOne() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Input number : ");
+        int number = sc.nextInt();
+
+        if (number >= 0) {
+            System.out.println("Your number >= 0");
+        }
+
+        if (number >= 10) {
+            System.out.println("Your number >= 10");
+        }
+
+        if (number >= 20) {
+            System.out.println("Your number >= 20");
+        }
+    }
+
+    private void avoidIfMoreThanOne() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Input number : ");
+        int number = sc.nextInt();
+
+        if ((number >= 0) && (number < 10)) {
+            System.out.println("Your number >= 0");
+        } else if ((number >= 10) && (number < 20)) {
+            System.out.println("Your number >= 10");
+        } else if ((number >= 20) && (number <= 30)) {
+            System.out.println("Your number >= 20");
+        } else {
+            System.out.println("Your number > 30");
         }
 
     }
