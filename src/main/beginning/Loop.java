ac3236b8159b720151add056f5e0ede552cc8fd9
Harin3Bone <harin.thananam@gmail.com>
2021-01-11 22:41:23 +0700

    Lesson6 - For loop (Increment)

--- src/main/beginning/Loop.java
+++ src/main/beginning/Loop.java
@@ -2,9 +2,13 @@ package main.beginning;
 
 public class Loop {
     public void runLoop() {
-        whileLoop();
-        whileWithBreak();
-        whileWithContinue();
+//        whileLoop();
+//        whileWithBreak();
+//        whileWithContinue();
+
+        forLoop();
+        forWithBreak();
+        forWithContinue();
     }
 
     private void whileLoop() {
@@ -15,19 +19,19 @@ public class Loop {
         }
     }
 
-    private void whileWithBreak(){
+    private void whileWithBreak() {
         int x = 1;
         while (true) {
             System.out.println("whileWithBreak - " + x);
 
-            if (x >= 9){
+            if (x >= 9) {
                 break;
             }
             x++;
         }
     }
 
-    private void whileWithContinue(){
+    private void whileWithContinue() {
         int x = 1;
         while (x < 10) {
             if (x == 5) {
@@ -38,6 +42,30 @@ public class Loop {
             System.out.println("whileWithContinue - " + x);
 
             x++;
+        }
+    }
+
+    private void forLoop() {
+        for (int x = 0; x <= 10; x++) {
+            System.out.println("ForLoop - " + x);
+        }
+    }
+
+    private void forWithBreak() {
+        for (int x = 0; x <= 10; x++) {
+            System.out.println("ForLoop - " + x);
+            if (x == 7) {
+                break;
+            }
+        }
+    }
+
+    private void forWithContinue() {
+        for (int x = 0; x <= 10; x++) {
+            if (x == 7) {
+                continue;
+            }
+            System.out.println("ForLoop - " + x);
         }
     }
 }
