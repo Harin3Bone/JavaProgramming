eb4f4156b1dcccb53f5b5bfa8d9f4b7f16a710b3
Harin3Bone <harin.thananam@gmail.com>
2021-01-11 21:51:56 +0700

    Assignment3 - Banknote calculate

--- src/main/JavaProgramming.java
+++ src/main/JavaProgramming.java
@@ -1,5 +1,6 @@
 package main;
 
+import main.assignment.BanknoteAmount;
 import main.assignment.BmiCalculator;
 import main.assignment.OddOrEven;
 import main.beginning.*;
@@ -23,16 +24,19 @@ public class JavaProgramming {
 //        Operator lesson4 = new Operator();
 //        lesson4.runOperator();
 
-//        Condition lesson5 = new Condition();
-//        lesson5.runCondition();
+        Condition lesson5 = new Condition();
+        lesson5.runCondition();
     }
 
     private static void assignment() {
 //        BmiCalculator assignment1 = new BmiCalculator();
 //        assignment1.runBmiCalculator();
 
-        OddOrEven assignment2 = new OddOrEven();
-        assignment2.runOddOrEven();
+//        OddOrEven assignment2 = new OddOrEven();
+//        assignment2.runOddOrEven();
+
+        BanknoteAmount assignment3 = new BanknoteAmount();
+        assignment3.runBankNoteAmount();
     }
 
 }
---
+++ src/main/assignment/BanknoteAmount.java
@@ -0,0 +1,59 @@
+package main.assignment;
+
+import java.util.Scanner;
+
+public class BanknoteAmount {
+    public void runBankNoteAmount() {
+        System.out.println("Banknote Amount");
+        System.out.println("-------------------------");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please input your money : ");
+        int get = sc.nextInt();
+        int count = 0;
+
+        if (get >= 1000) {
+            count = get / 1000;
+            get = get % 1000;
+            System.out.println("Bank 1000 : " + count + " paper");
+        }
+
+        if (get >= 500) {
+            count = get / 500;
+            get = get % 500;
+            System.out.println("Bank 500 : " + count + " paper");
+        }
+
+        if (get >= 100) {
+            count = get / 100;
+            get = get % 100;
+            System.out.println("Bank 100 : " + count + " paper");
+        }
+
+        if (get >= 50) {
+            count = get / 50;
+            get = get % 50;
+            System.out.println("Bank 50 : " + count + " paper");
+        }
+
+        if (get >= 20) {
+            count = get / 20;
+            get = get % 20;
+            System.out.println("Bank 20 : " + count + " paper");
+        }
+
+        if (get >= 10) {
+            count = get / 10;
+            get = get % 10;
+            System.out.println("Coin 10 : " + count + " paper");
+        }
+
+        if (get >= 5) {
+            count = get / 5;
+            get = get % 5;
+            System.out.println("Coin 5 : " + count + " paper");
+            System.out.println("Coin 1 : " + get + " paper");
+        }
+
+    }
+}
